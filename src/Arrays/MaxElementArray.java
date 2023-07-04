b9fc797789c703816caa8fdef7817c27d4863880
Rudraksh <vatsrudraksh@gmail.com>
2023-07-04 22:18:57 +0530

    Commit

---
+++ src/Arrays/MaxElementArray.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class MaxElementArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array");
+        int arraySize = scanner.nextInt();
+        Integer[] integerArray = new Integer[arraySize];
+        System.out.print("Enter the elements of the array");
+        for (int i = 0; i < integerArray.length; i++) {
+            integerArray[i] = scanner.nextInt();
+        }
+        int maxElementPosition = 0;
+        for (int i = 0; i < integerArray.length; i++) {
+            Integer number = integerArray[i];
+            if (number > integerArray[maxElementPosition]) {
+                maxElementPosition = i;
+            }
+        }
+        System.out.println("Maximum element of the array is: " + integerArray[maxElementPosition]);
+    }
+}
