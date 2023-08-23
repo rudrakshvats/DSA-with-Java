23a4a5c12a2e4a966ebf4fef4108682b8ac3a9d0
Rudraksh Vats <rudraksh.vats@satincreditcare.com>
2023-08-23 14:32:21 +0530

    Commit

--- src/Factorial.java
+++ src/Factorial.java
@@ -6,6 +6,28 @@ public class Factorial {
         System.out.print("Enter the number to calculate its factorial: ");
         Integer number = scanner.nextInt();
         System.out.println("The factorial for " + number + " = " + iterativeFactorial(number));
+        System.out.println("The factorial for " + number + " = " + recursiveFactorial(number));
+    }
+
+    /**
+     * Recursive factorial method will use the concept of recursion which method calling itself. So to calculate
+     * factorial of number, we will multiply the number to the factorial of number - 1 i.e. if you want to calculate 2!
+     * then we will calculate 2 * 1!, for 1! we will calculate 1 * 0! which is 1.
+     * @param number
+     * @return
+     */
+    private static Integer recursiveFactorial(Integer number) {
+        /*
+        this is the breaking condition which is necessary else recursive flow will not break hence producing
+        StackOverflow error. Why this exception? Because recursion happens in stack memory i.e. call stack which performs
+        LIFO operations. So once our breaking condition is executed, our call stack comes in place and once call stack
+        is empty we get a return value.
+         */
+        if (number == 0) {
+            return 1;
+        }
+
+        return number * recursiveFactorial(number - 1);
     }
 
     /**
