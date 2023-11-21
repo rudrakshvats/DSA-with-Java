aff1c91f237e8a308dc33845dce0da4c1d09a2e1
Rudrakash.Vats <rudraksh.vats@satincreditcare.com>
2023-11-21 21:59:59 +0530

    Pattern 5

--- src/PepCoding/Patterns/Pattern5.java
+++ src/PepCoding/Patterns/Pattern5.java
@@ -7,9 +7,28 @@ public class Pattern5 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number to print the pattern: ");
         int n = scanner.nextInt();
+        int spaces = n / 2;
+        int stars = 1;
+        /*
+        in this pattern the spaces decrease one by one till we reach n/2 + 1 value and
+        stars increase by 2 till we reach n/2 + 1 and as we move from n/2 + 1 to n,
+        spaces increase one by one and stars decrease by 2.
+         */
         for (int i = 1; i <= n; i++) {
-            // first and last line of the pattern will have only one star and n/2 spaces before the star
-            for (int j = )
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("* \t");
+            }
+            if (i <= n / 2) {
+                spaces -= 1;
+                stars += 2;
+            }
+            else {
+                spaces += 1;
+                stars -= 2;
+            }
             System.out.print("\n");
         }
     }
