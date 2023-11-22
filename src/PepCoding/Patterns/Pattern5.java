fc78560eb2fe60bc5730485bc95d6a1f00267828
Rudrakash.Vats <rudraksh.vats@satincreditcare.com>
2023-11-22 17:38:43 +0530

    Pattern 5 and 6

--- src/PepCoding/Patterns/Pattern5.java
+++ src/PepCoding/Patterns/Pattern5.java
@@ -7,7 +7,7 @@ public class Pattern5 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number to print the pattern: ");
         int n = scanner.nextInt();
-        int spaces = n / 2;
+        int spaces = n / 2; // for this type of pattern we will notice that the first row always contain n/2 spaces and 1 star
         int stars = 1;
         /*
         in this pattern the spaces decrease one by one till we reach n/2 + 1 value and
---
+++ src/PepCoding/Patterns/Pattern6.java
@@ -0,0 +1,33 @@
+package PepCoding.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern6 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to print the pattern: ");
+        int n = scanner.nextInt();
+        int spaces = 1;
+        int stars = n - 2;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("* \t");
+            }
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("\t");
+            }
+            /*for (int j = spaces + stars; j <= n + 1; j++) {
+                System.out.print("* \t");
+            }*/
+            if (i <= n / 2) {
+                spaces += 2;
+                stars -= 1;
+            }
+            else {
+                spaces -= 2;
+                stars += 1;
+            }
+            System.out.print("\n");
+        }
+    }
+}
