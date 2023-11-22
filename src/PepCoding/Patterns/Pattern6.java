d0a597e755fae1892f17ba2c80437aa8c4ae12b4
Rudraksh <vatsrudraksh@gmail.com>
2023-11-22 21:50:48 +0530

    Pattern 6

--- src/PepCoding/Patterns/Pattern6.java
+++ src/PepCoding/Patterns/Pattern6.java
@@ -8,7 +8,7 @@ public class Pattern6 {
         System.out.print("Enter a number to print the pattern: ");
         int n = scanner.nextInt();
         int spaces = 1;
-        int stars = n - 2;
+        int stars = (n / 2) + 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= stars; j++) {
                 System.out.print("* \t");
@@ -16,9 +16,9 @@ public class Pattern6 {
             for (int j = 1; j <= spaces; j++) {
                 System.out.print("\t");
             }
-            /*for (int j = spaces + stars; j <= n + 1; j++) {
+            for (int j = spaces + stars; j <= n + 1; j++) {
                 System.out.print("* \t");
-            }*/
+            }
             if (i <= n / 2) {
                 spaces += 2;
                 stars -= 1;
