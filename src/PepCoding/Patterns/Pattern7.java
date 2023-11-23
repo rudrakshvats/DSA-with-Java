2d6578687737ed562c368dd307b096100357a8b9
Rudraksh <vatsrudraksh@gmail.com>
2023-11-23 21:56:14 +0530

    Pattern 7

---
+++ src/PepCoding/Patterns/Pattern7.java
@@ -0,0 +1,18 @@
+package PepCoding.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to print the pattern: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("\t");
+            }
+            System.out.println("* \t");
+        }
+
+    }
+}
