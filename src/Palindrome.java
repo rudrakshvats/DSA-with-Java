1f161bfaa40f61ed3dca46f24f3db0a00631dfed
rudraksh.vats <vatsrudraksh@gmail.com>
2026-02-10 00:52:15 +0530

    Palindrome

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -11,20 +11,30 @@ public class Palindrome {
     private static void checkIsNumberPalindrome(Integer number) {
         int original = number;
         int reverse = 0;
-        if (number > 0 && number < 10) {
+
+        // for single digit check
+        if (number >= 0 && number < 10) {
             System.out.println("Single digit numbers are already palindrome");
         }
-        while (number > 0) {
-            int remainder = number % 10;
-            reverse = reverse * 10 + remainder;
-            number = number / 10;
-        }
 
-        if (original == reverse) {
-            System.out.println(original + " is a palindrome number");
+        // for any number which has 0 at one's digit
+        if (number % 10 == 0) {
+            System.out.println(original + " is not a palindrome number");
         }
         else {
-            System.out.println(original + " is not a palindrome number");
+            while (number > 0) {
+                int remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+                number = number / 10;
+            }
+
+            if ((original == reverse) || (original == reverse / 10)) {
+                // here the first check will be successful for even number of digit and for odd number of digit just divide the number by 10 so that the middle digit of
+                // the number which is kind of unrequired is ignored
+                System.out.println(original + " is a palindrome number");
+            } else {
+                System.out.println(original + " is not a palindrome number");
+            }
         }
     }
 }
