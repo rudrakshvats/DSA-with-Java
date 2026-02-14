f21b6780d1d7c80e8ac184fa95a0792beee8853f
rudraksh.vats <vatsrudraksh@gmail.com>
2026-02-15 01:52:16 +0530

    RomanNumeralToInteger

--- src/RomanNumeralToInteger.java
+++ src/RomanNumeralToInteger.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class RomanNumeralToInteger {
@@ -5,66 +7,30 @@ public class RomanNumeralToInteger {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a Roman numeral: ");
         String romanNumeral = scanner.next();
-        int current = 0;
-        Integer answer = 0;
-        int previous = 0;
+        System.out.println(romanToInteger(romanNumeral));
+    }
 
-        // read the string from right to left
-        for (int i = romanNumeral.length() - 1; i >= 0; i--) {
-            switch (romanNumeral.charAt(i)) {
-                case 'I':
-                    current = 1;
-                    break;
-                case 'V':
-                    current = 5;
-                    break;
-                case 'X':
-                    current = 10;
-                    break;
-                case 'L':
-                    current = 50;
-                    break;
-                case 'C':
-                    current = 100;
-                    break;
-                case 'D':
-                    current = 500;
-                    break;
-                case 'M':
-                    current = 1000;
-                    break;
-            }
-            /*
-            2. since the value of previous is 5 and in next iteration the value of current becomes 1 so, 5 > 1, thus
-            the answer = answer - 1 = 4, which is the value of IV so next in the next iteration previous remains 5
-            and now current becomes C which is 100 so it will go to else case as 5 < 100
-            4. here previous > current so answer = answer - 10 = 94, now in next iteration current becomes M i.e.
-            1000 so, 100 < 1000 so it will go to else case
-            6. here 1000 > 100, so answer = answer - 100 = 994 and current will become 1000 so now 100 < 1000 so it will
-            go to else case
-             */
-            if (previous > current) {
-                answer -= current;
-            }
-            /*
-             1. here we read the string from right to left in order to see that values like IV or IX are present or not,
-            if they are present then we take two variables, current and previous, from right to left if the value is
-            read and if suppose we encounter a case like IV then first we will add value of V i.e. 5 to answer, now
-            in the next iteration the value of current will change to value of I which is 1 and previous value will
-            be 5
-            3. since in next iteration previous < current, so we increment answer = answer + 100 = 104 and now previous = 100
-            and iteration makes current value X which 10, now 100 > 10 so it will go to above case
-            5. here 100 < 1000 so, answer = answer + 1000 = 1094, now previous = 1000 and current will be 100 so it will go to
-            above if case
-            7. here 100 < 1000 so, answer = answer +  1000 = 1994, now previous = 1000 and there is no current as we will hit the
-            back of char array so final answer is 1994
-             */
-            else {
-                answer += current;
-                previous = current;
+    // here we will iterate from left to right, we can use a separate method to store values corresponding to each roman character or we can use a map
+    // we will check that the value of first character is less than the value of second character, if yes then value of first character will be decremented from result
+    // else we will increment it, this process will continue until we reach the second last character as the last character value will always be positive
+    private static int romanToInteger(String romanNumber) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int result = 0;
+        for (int i = 0; i < romanNumber.length(); i++) {
+            if (i < romanNumber.length() - 1 && map.get(romanNumber.charAt(i)) < map.get(romanNumber.charAt(i + 1))) {
+                result -= map.get(romanNumber.charAt(i));
+            } else {
+                result += map.get(romanNumber.charAt(i));
             }
         }
 
-        System.out.println(answer);
+        return result;
     }
 }
