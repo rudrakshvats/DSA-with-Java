cb8e015f2d58d3954a374a7feb8c55afae6e9ad5
Rudraksh <vatsrudraksh@gmail.com>
2026-03-28 13:14:17 +0530

    GFG Problems

---
+++ src/PrintSquare.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrintSquare {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n) {
+                    System.out.print("* ");
+                } else {
+                    if (j == 1 || j == n) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
