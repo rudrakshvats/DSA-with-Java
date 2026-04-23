ce24a482235548ab889e963310cb03e2db6214a3
rudraksh.vats <vatsrudraksh@gmail.com>
2026-04-23 10:15:29 +0530

    BinarySearch and SearchInsertPosition

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -4,16 +4,19 @@ public class BinarySearch {
         int low = 0;
         int high = arr.length - 1;
         int target = 23;
-        System.out.print(binarySearchUsingRecursion(arr, low, high, target));
+        System.out.println(binarySearchUsingRecursion(arr, low, high, target));
     }
 
     private static int binarySearchUsingRecursion(int[] arr, int low, int high, int target) {
         if (low < high) {
             int mid = (low + high) / 2;
 
+            // if we found the target at the mid index value, then simply return mid value, which is actually the break condition for recursive function
             if (arr[mid] == target) return mid;
 
+            // if target is greater than element at mid index then simply, assign the low value to mid + 1 value and keep the high same
             if (arr[mid] < target) return binarySearchUsingRecursion(arr, mid + 1, high, target);
+            // else if target is less than element at mid index then low becomes 0 and high becomes mid - 1
             else return binarySearchUsingRecursion(arr, 0, mid - 1, target);
         }
 
--- src/LeetCode/SearchInsertPosition.java
+++ src/LeetCode/SearchInsertPosition.java
@@ -2,29 +2,37 @@ package LeetCode;
 
 public class SearchInsertPosition {
     public static void main(String[] args) {
-        int[] nums = {1, 3, 5};
-        int target = 4;
+        int[] nums = {1, 3};
+        int target = 1;
         System.out.println(searchInsert(nums, target));
     }
 
     public static int searchInsert(int[] nums, int target) {
+        // if the nums array is empty then simply return 0
         if (nums.length == 0) return 0;
+        // if the nums array is of length 1
         if (nums.length == 1) {
+            // check if the only element is equal to target or not, if yes then 0 is the answer as the target is the only element in the array
             if (nums[0] == target) return 0;
+            // else if the target is greater than only element in the array, then obviously it will be present at 1
             else if (nums[0] < target) return 1;
+            // else just return 0
             else return 0;
         }
+        // we will use two pointer approach in this problem
         int leftPointer = 0;
         int rightPointer = nums.length - 1;
-        while (leftPointer < rightPointer) {
-            if (nums[leftPointer] > target) {
-                return leftPointer;
-            } else if (nums[leftPointer] < target) {
+        // check if target is less than the first element of the array or not, if yes then return 0
+        if (target < nums[leftPointer]) return leftPointer;
+        // else just return the length of nums array, because it will always be greater than last element's index value
+        if (target > nums[rightPointer]) return nums.length;
+        // we will use two pointer here and loop the condition if the target is less than element at leftPointer and greater than equal to element at
+        // rightPointer, greater than equal to because the target can be equal to the last element as well, and we are not doing the same for leftPointer because
+        // we are returning leftPointer as the final answer
+        while (target > nums[leftPointer] && target <= nums[rightPointer]) {
+            if (target > nums[leftPointer]) {
                 leftPointer += 1;
-            }
-            if (nums[rightPointer] < target) {
-                return nums.length;
-            } else if (nums[rightPointer] > target) {
+            } else if (target < nums[rightPointer]) {
                 rightPointer -= 1;
             }
         }
