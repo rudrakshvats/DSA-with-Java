76fe740e443c91def1ad4b12169c97cc1c2c2dc0
rudraksh.vats <rudraksh.vats@sbs-software.com>
2026-06-01 15:08:41 +0530

    LeetCode DSA

--- src/LeetCode/ValidParentheses.java
+++ src/LeetCode/ValidParentheses.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class ValidParentheses {
     public static void main(String[] args) {
@@ -8,35 +9,61 @@ public class ValidParentheses {
         System.out.println(isValid(s));
     }
 
-    public static boolean isValid(String s) {
-        String[] strArray = s.split("");
-        boolean isValid = false;
+//    public static boolean isValid(String s) {
+//        String[] strArray = s.split("");
+//        boolean isValid = false;
+//
+//        for (String str : strArray) {
+//            if (s.isEmpty()) return isValid;
+//            if (str.equals("(") && Arrays.asList(strArray).contains(")")) {
+//                isValid = true;
+//                s = s.replace("(", "").replace(")", "");
+//            } else if (str.equals(")") && Arrays.asList(strArray).contains("(")) {
+//                isValid = true;
+//                s = s.replace("(", "").replace(")", "");
+//            } else if (str.equals("{") && Arrays.asList(strArray).contains("}")) {
+//                isValid = true;
+//                s = s.replace("{", "").replace("}", "");
+//            } else if (str.equals("}") && Arrays.asList(strArray).contains("{")) {
+//                isValid = true;
+//                s = s.replace("{", "").replace("}", "");
+//            } else if (str.equals("[") && Arrays.asList(strArray).contains("]")) {
+//                isValid = true;
+//                s = s.replace("[", "").replace("]", "");
+//            } else if (str.equals("]") && Arrays.asList(strArray).contains("[")) {
+//                isValid = true;
+//                s = s.replace("[", "").replace("]", "");
+//            } else {
+//                isValid = false;
+//            }
+//        }
+//
+//        return isValid;
+//    }
 
-        for (String str : strArray) {
-            if (s.isEmpty()) return isValid;
-            if (str.equals("(") && Arrays.asList(strArray).contains(")")) {
-                isValid = true;
-                s = s.replace("(", "").replace(")", "");
-            } else if (str.equals(")") && Arrays.asList(strArray).contains("(")) {
-                isValid = true;
-                s = s.replace("(", "").replace(")", "");
-            } else if (str.equals("{") && Arrays.asList(strArray).contains("}")) {
-                isValid = true;
-                s = s.replace("{", "").replace("}", "");
-            } else if (str.equals("}") && Arrays.asList(strArray).contains("{")) {
-                isValid = true;
-                s = s.replace("{", "").replace("}", "");
-            } else if (str.equals("[") && Arrays.asList(strArray).contains("]")) {
-                isValid = true;
-                s = s.replace("[", "").replace("]", "");
-            } else if (str.equals("]") && Arrays.asList(strArray).contains("[")) {
-                isValid = true;
-                s = s.replace("[", "").replace("]", "");
-            } else {
-                isValid = false;
+    public static boolean isValid(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char ch: s.toCharArray()) {
+            if (deque.isEmpty() && isOpeningBracket(ch)) deque.push(ch);
+            else {
+                if (!deque.isEmpty() && isClosingBracket(ch)) {
+                    if (ch == ')' && deque.peek() != '(') return false;
+                    else if (ch == ']' && deque.peek() != '[') return false;
+                    else if (ch == '}' && deque.peek() != '{') return false;
+                    else deque.pop();
+                } else deque.push(ch);
             }
         }
 
-        return isValid;
+        return deque.isEmpty();
+    }
+
+    private static boolean isOpeningBracket(char ch) {
+        return ch == '(' || ch == '[' || ch == '{';
+    }
+
+    private static boolean isClosingBracket(char ch) {
+        return ch == ')' || ch == ']' || ch == '}';
     }
 }
